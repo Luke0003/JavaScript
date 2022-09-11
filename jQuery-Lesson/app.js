e0cfa7bdf1c627f2f70698a6c6c82be711683831
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2022-09-11 14:37:43 +0000

    [Finish]6章

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,10 +1,8 @@
-$(function (){
-  $('.box1').slideDown(2000, function(){
-    $('.box1').css({
-      'background-color': '#0000FF',
-      'width': '200px',
-      'height': '100px'
-    })
-    $('.box1').slideUp(2000);
+$(document).ready(function(){
+  $(".box1").on("click", function(){
+    $(".box1").addClass("box1-ext");
+  });
+  $(".box1").mouseout(function(){
+    $(".box1").removeClass("box1-ext");
   });
 });
